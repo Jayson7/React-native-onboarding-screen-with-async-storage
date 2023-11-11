492717a4773d3ab4d78a68c3d85de00a79187c40
jayson7 <lexxiijoo70@gmail.com>
2023-11-11 18:17:32 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import OnboardingScreen from "./core/onboard/onBoardingScreen";
+import HomeScreen from "./core/home/homepage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+
+  React.useEffect(() => {
+    const checkIfFirstLaunch = async () => {
+      try {
+        const appData = await AsyncStorage.getItem("isAppFirstLaunched");
+
+        if (appData === null) {
+          // App is launched for the first time
+          await AsyncStorage.setItem("isAppFirstLaunched", "true");
+          setIsAppFirstLaunched(true);
+        } else {
+          // App is not launched for the first time
+          setIsAppFirstLaunched(false);
+        }
+      } catch (error) {
+        // Handle error
+        console.error("Error checking if app is first launched:", error);
+      }
+    };
+
+    checkIfFirstLaunch();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    isAppFirstLaunched != null && (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {isAppFirstLaunched && (
+            <Stack.Screen
+              name="OnboardingScreen"
+              component={OnboardingScreen}
+            />
+          )}
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
